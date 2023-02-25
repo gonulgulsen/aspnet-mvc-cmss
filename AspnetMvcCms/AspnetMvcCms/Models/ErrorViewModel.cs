f755709228d7b110a848033330d1fe4428126659
gonulgulsen <115918601+gonulgulsen@users.noreply.github.com>
2023-02-25 22:08:55 +0300

    deneme

---
+++ AspnetMvcCms/AspnetMvcCms/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspnetMvcCms.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
